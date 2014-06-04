73a639e495e053b5506a6aabea5ecb590bed4b1f
Paolo Ibarra <jpibarra1130@gmail.com>
2014-06-04 21:51:46 +0800

    Initial Commit

---
+++ swift-exploration/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  swift-exploration
+//
+//  Created by Paolo Ibarra on 4/6/14.
+//  Copyright (c) 2014 Paolo Ibarra. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
